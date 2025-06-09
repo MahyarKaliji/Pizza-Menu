e13f4275612772ccc18614811367159710372446
Mahyar Kaliji <mahyar.kaliji@gmail.com>
2025-06-09 15:51:30 +0330

    installed react router to the project and create AppLayout component

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Template Pizza Co.</Link>
+
+      <p>Mahyar</p>
+    </header>
+  );
+}
+
+export default Header;
