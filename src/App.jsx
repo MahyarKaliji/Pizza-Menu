db697afaffdf6269b04165ff2564cd0756099a1f
Mahyar Kaliji <mahyar.kaliji@gmail.com>
2025-06-10 13:13:02 +0330

    added orderLoader for recieve order details, added form action for create new order, added SearchOrder component for search with order Id, and handled error for phone number on new order

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import Order from "./features/order/Order";
-import CreateOrder from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
+import CreateOrder, {
+  action as orderAction,
+} from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -19,6 +23,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
@@ -27,10 +32,13 @@ const router = createBrowserRouter([
       {
         path: "/order/new",
         element: <CreateOrder />,
+        action: orderAction,
       },
       {
-        path: "/order/:orderID",
+        path: "/order/:orderId",
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
